1173fd2e79f3fdea5b73bfcffa84690a72fa4cd0
callendera <acallender@live.sscc.edu>
2022-09-15 22:57:27 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int numToExamine = 19;
+
+        if (numToExamine % 2 == 0) {
+            System.out.println ("This is an Even integer.");
+        }
+        else
+        {
+            System.out.println ("This is an Odd integer.");
+        }
+
+    }
+}
